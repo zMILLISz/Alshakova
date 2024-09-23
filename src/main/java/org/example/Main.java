6ae1abff9e5b018619a392f5e944bd3ba6f5218a
Миллис <juliypuly@gmail.com>
2024-09-23 14:53:23 +0300

    second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,10 +11,5 @@ public class Main {
         System.out.printf(StringUtils.upperCase("Hello and welcome!"));
 
 
-//        for (int i = 1; i <= 5; i++) {
-//            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-//            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-//            System.out.println("i = " + i);
-
     }
 }
