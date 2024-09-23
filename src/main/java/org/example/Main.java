dc0bd8c4682a6a4cf49f5eebcae8706d98e5190c
Миллис <juliypuly@gmail.com>
2024-09-23 15:05:06 +0300

    Fixes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf(StringUtils.upperCase("Hello and welcome!"));
+        System.out.printf(StringUtils.upperCase("Hello and welcome"));
 
 
     }
